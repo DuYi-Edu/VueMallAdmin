7399e45b39ec84ac7f0362b276968c28236af169
king <yongzhi.ding@duyi-inc.com>
2020-08-19 15:17:03 +0800

    菜单栏渲染

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ const ayncRouterMap = [{
   name: 'Product',
   meta: {
     title: '商品',
+    icon: 'inbox',
+    hidden: false,
   },
   component: Home,
   children: [{
@@ -18,6 +20,8 @@ const ayncRouterMap = [{
     name: 'ProductList',
     meta: {
       title: '商品列表',
+      icon: 'unordered-list',
+      hidden: false,
     },
     component: () => import('@/views/page/productList.vue'),
   }, {
@@ -25,6 +29,8 @@ const ayncRouterMap = [{
     name: 'ProductAdd',
     meta: {
       title: '添加商品',
+      icon: 'file-add',
+      hidden: false,
     },
     component: () => import('@/views/page/productAdd.vue'),
   }, {
@@ -32,6 +38,8 @@ const ayncRouterMap = [{
     name: 'Category',
     meta: {
       title: '类目管理',
+      icon: 'project',
+      hidden: false,
     },
     component: () => import('@/views/page/category.vue'),
   }],
@@ -44,12 +52,16 @@ const routes = [
     component: Home,
     meta: {
       title: '首页',
+      hidden: false,
+      icon: 'home',
     },
     children: [{
       path: 'index',
       name: 'Index',
       meta: {
         title: '统计',
+        icon: 'number',
+        hidden: false,
       },
       component: () => import('../views/page/index.vue'),
     }],
@@ -60,6 +72,7 @@ const routes = [
     component: Login,
     meta: {
       title: '登录',
+      hidden: true,
     },
   },
 ];
@@ -73,8 +86,10 @@ router.beforeEach((to, from, next) => {
     if (store.state.user.appkey && store.state.user.username && store.state.user.role) {
       if (!isAddRoutes) {
         const menuRoutes = getMenuRoutes(store.state.user.role, ayncRouterMap);
-        router.addRoutes(menuRoutes);
-        store.dispatch('changeMenuRoutes', routes.concat(menuRoutes));
+        store.dispatch('changeMenuRoutes', routes.concat(menuRoutes)).then(() => {
+          router.addRoutes(menuRoutes);
+          next();
+        });
         isAddRoutes = true;
       }
       return next();
--- src/views/layout/components/menu.vue
+++ src/views/layout/components/menu.vue
@@ -1,26 +1,38 @@
 <template>
   <div class="menu-list">
     <a-menu
-      :default-selected-keys="['1']"
-      :default-open-keys="['sub1']"
+      :default-selected-keys="[$router.currentRoute.matched[1] ?
+                                $router.currentRoute.matched[1].name :
+                                '']"
+      :default-open-keys="[$router.currentRoute.matched[0].name]"
       mode="inline"
       theme="dark"
       :inline-collapsed="$store.state.collapsed"
     >
-      <a-sub-menu v-for="route in $store.state.menuRoutes" :key="route.name">
-        <span slot="title">
-          <a-icon type="mail" />
+    <template v-for="route in $store.state.menuRoutes">
+       <a-sub-menu v-if="!route.meta.hidden" :key="route.name">
+          <span slot="title">
+          <a-icon :type="route.meta.icon" />
           <span>{{route.meta.title}}</span>
         </span>
-        <a-menu-item v-for="child in route.children" :key="child.name">
-          {{child.meta.title}}
+        <a-menu-item  v-for="child in route.children" :key="child.name">
+          <router-link :to="{name: child.name}">
+            <a-icon :type="child.meta.icon" />
+            {{child.meta.title}}
+          </router-link>
         </a-menu-item>
-
       </a-sub-menu>
-
+    </template>
     </a-menu>
   </div>
 </template>
+<script>
+export default {
+  created() {
+    console.log(this.$router);
+  },
+};
+</script>
 <style lang="less">
 .menu-list {
   width: 200px;
