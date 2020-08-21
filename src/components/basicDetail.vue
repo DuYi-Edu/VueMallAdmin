f72a7c53b43f1e919ede4cbbc1105ff5bcdd421b
king <yongzhi.ding@duyi-inc.com>
2020-08-21 17:47:00 +0800

    新增商品基本信息的填写

---
+++ src/components/basicDetail.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="basic-detail">
+    <a-form-model ref="ruleForm" :model="form" :rules="rules"
+        :label-col="{span: 5}" :wrapper-col="{span: 13}">
+      <a-form-model-item label="标题" prop="title" required>
+        <a-input v-model="form.title" />
+      </a-form-model-item>
+      <a-form-model-item label="商品描述" prop="desc">
+        <a-input v-model="form.desc" />
+      </a-form-model-item>
+      <a-form-model-item label="商品类目" required prop="category">
+        <a-select v-model="form.category" placeholder="请选择商品类别" @change="changeCategory">
+          <a-select-option v-for="c in categoryList" :key="c.id" :value="c.id"
+                           >
+              {{c.name}}
+            </a-select-option>
+        </a-select>
+        <a-select v-model="form.c_items" placeholder="请添加子类目">
+          <a-select-option v-for="c in categoryItems" :key="c" :value="c">
+            {{c}}
+        </a-select-option>
+        </a-select>
+      </a-form-model-item>
+      <a-form-model-item label="商品标签" prop="tags" required>
+        <a-select
+          mode="tags"
+          placeholder="Please select"
+          :default-value="['包邮，最晚次日达']"
+          v-model="form.tags"
+        >
+          <a-select-option
+            value="包邮，最晚次日达"
+          >包邮，最晚次日达</a-select-option>
+        </a-select>
+      </a-form-model-item>
+      <a-form-model-item label=" " class="next-btn">
+          <a-button type="primary" @click="next">下一步</a-button>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+
+<script>
+import categoryApi from '@/api/category';
+
+export default {
+  data() {
+    return {
+      form: {
+        title: '',
+        desc: '',
+        category: '',
+        c_items: [],
+        tags: [],
+      },
+      rules: {
+
+      },
+      categoryList: [],
+      categoryItems: [],
+    };
+  },
+  created() {
+    categoryApi.list().then((res) => {
+      this.categoryList = res.data;
+    });
+  },
+  methods: {
+    changeCategory(category) {
+      for (let i = 0; i < this.categoryList.length; i += 1) {
+        if (this.categoryList[i].id === category) {
+          this.categoryItems = this.categoryList[i].c_items;
+        }
+      }
+    },
+    next() {
+      this.$refs.ruleForm.validate((valid) => {
+        if (valid) {
+          this.$emit('next', this.form);
+          console.log(this.form);
+          return true;
+        }
+        console.log('error submit!!');
+        return false;
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less">
+    .basic-detail {
+        .next-btn {
+            text-align: center;
+        }
+    }
+
+</style>
--- src/views/page/productAdd.vue
+++ src/views/page/productAdd.vue
@@ -1,3 +1,66 @@
 <template>
-    <div>新增商品</div>
+  <div class="product-detail">
+    <a-steps :current="current" class="product-steps">
+      <a-step v-for="item in steps" :key="item.title" :title="item.title" />
+    </a-steps>
+    <div class="steps-content">
+      <basic-info v-if="current === 0" @next="next"/>
+      <sale-info v-else-if="current === 1" />
+    </div>
+  </div>
 </template>
+
+<script>
+import BasicInfo from '@/components/basicDetail.vue';
+import SaleInfo from '@/components/saleDetail.vue';
+
+export default {
+  data() {
+    return {
+      current: 0,
+      steps: [
+        {
+          title: '填写商品基本信息',
+        },
+        {
+          title: '填写商品销售信息',
+        },
+      ],
+    };
+  },
+  components: {
+    BasicInfo,
+    SaleInfo,
+  },
+  methods: {
+    next(form) {
+      this.current += 1;
+      console.log(form);
+    },
+    prev() {
+      this.current -= 1;
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.product-detail {
+    .product-steps {
+        width: 50%;
+        margin: 20px auto;
+    }
+  .steps-content {
+    margin-top: 16px;
+    border: 1px dashed #e9e9e9;
+    border-radius: 6px;
+    background-color: #fafafa;
+    min-height: 200px;
+    text-align: center;
+    padding-top: 80px;
+  }
+
+  .steps-action {
+    margin-top: 24px;
+  }
+}
+</style>
